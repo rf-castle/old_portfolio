0812bb0565754d566a0bdf1a79163560af23b6bd
rf-castle <rf-castle@users.noreply.github.com>
2021-07-01 06:03:08 +0000

    deploy: 396bdbed8e9b764ba328bd59f9c6c762185f0fd7

---
+++ _nuxt/d9d3458.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{335:function(n,t,e){"use strict";e.r(t);var c={name:"contact"},o=e(76),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,"46c53c88",null);t.default=component.exports}}]);
